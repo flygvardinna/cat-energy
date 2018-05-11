a6180d340f133a60acb8f0607bfa1bdd5229a1c3
Оксана <zhuk11oxana@gmail.com>
2018-05-11 13:28:09 +0700

    js для кота, правка иконок в форме на планшете

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,9 @@
 var pageHeader = document.querySelector('.page-header');
 var navToggle = document.querySelector('.page-header__nav-toggle');
+var catPicture = document.querySelector('.before-after__cat-picture');
+var catFatButton = document.querySelector('.before-after__button--before');
+var catSlimButton = document.querySelector('.before-after__button--after');
+var catToggle = document.querySelector('.before-after__toggle');
 
 pageHeader.classList.remove('page-header--nojs');
 
@@ -10,5 +14,29 @@ navToggle.addEventListener('click', function() {
   } else {
     pageHeader.classList.add('page-header--nav-closed');
     pageHeader.classList.remove('page-header--nav-opened');
+  }
+});
+
+catFatButton.addEventListener('click', function() {
+  if (catPicture.classList.contains('before-after__cat-slim')) {
+    catPicture.classList.remove('before-after__cat-slim');
+    catPicture.classList.add('before-after__cat-fat');
+  }
+});
+
+catSlimButton.addEventListener('click', function() {
+  if (catPicture.classList.contains('before-after__cat-fat')) {
+    catPicture.classList.remove('before-after__cat-fat');
+    catPicture.classList.add('before-after__cat-slim');
+  }
+});
+
+catToggle.addEventListener('click', function() {
+  if (catPicture.classList.contains('before-after__cat-fat')) {
+    catPicture.classList.remove('before-after__cat-fat');
+    catPicture.classList.add('before-after__cat-slim');
+  } else {
+    catPicture.classList.add('before-after__cat-fat');
+    catPicture.classList.remove('before-after__cat-slim');
   }
 });
